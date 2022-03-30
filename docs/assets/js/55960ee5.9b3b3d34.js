997dac1e1917d6d8a38bb62abebd4ce0fc3ae3f7
xiangnan Liu <xiangnan.liu@grabtaxi.com>
2022-03-30 17:22:18 +0800

    move all file to root

--- docs/assets/js/55960ee5.9b3b3d34.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkblog_website=self.webpackChunkblog_website||[]).push([[4121],{8070:function(e){e.exports=JSON.parse('[{"name":"\u6f14\u793a","permalink":"/docs/tags/\u6f14\u793a","count":1},{"name":"\u5f00\u59cb","permalink":"/docs/tags/\u5f00\u59cb","count":1}]')}}]);
